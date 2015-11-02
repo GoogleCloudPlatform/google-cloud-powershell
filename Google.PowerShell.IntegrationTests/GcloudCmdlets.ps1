7d3c6f636d6757d3658c927b335a6cd051f68b74
Chris Smith <chrsmith@google.com>
2015-11-02 10:35:16 -0800

    Add integeration tests

---
+++ Google.PowerShell.IntegrationTests/GcloudCmdlets.ps1
@@ -0,0 +1,7 @@
+﻿# Install the GCP cmdlets module into the current PowerShell session.
+function Install-GcloudCmdlets() {
+	# TODO(chrsmith): Check both the Debug and Release, use most recent.
+	$pathToCmdlets = "$PSSCriptRoot\..\Google.PowerShell\bin\Debug\Google.PowerShell.dll"
+	Import-Module $pathToCmdlets -Verbose
+}
+
