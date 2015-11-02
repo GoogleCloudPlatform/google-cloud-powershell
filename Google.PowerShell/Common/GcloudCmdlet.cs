215fba65a90ba3eb1d52fce74b99f804b946e1a3
Chris Smith <chrsmith@google.com>
2015-11-02 09:47:42 -0800

    Source code

---
+++ Google.PowerShell/Common/GcloudCmdlet.cs
@@ -0,0 +1,63 @@
+﻿// Copyright 2015 Google Inc. All Rights Reserved.
+
+using System.Management.Automation;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using System.Threading.Tasks;
+
+namespace Google.PowerShell.Common
+{
+    /// <summary>
+    /// Base commandlet for all Google Cloud cmdlets.
+    /// </summary>
+    public abstract class GCloudCmdlet : PSCmdlet
+    {
+        public GCloudCmdlet()
+        {
+            CloudSdk = new CloudSdkSettings();
+        }
+
+        public CloudSdkSettings CloudSdk { get; protected set; }
+
+        /// <summary>
+        /// Returns an instance of the Google Client API initializer, using the machine's default credentials.
+        /// </summary>
+        protected BaseClientService.Initializer GetBaseClientServiceInitializer()
+        {
+            // TODO(chrsmith): Support the notion of "profiles" and switching between them.
+            // This should be built into the CloudSdkSettings class.
+            Task<GoogleCredential> getCredsTask = GoogleCredential.GetApplicationDefaultAsync();
+            getCredsTask.Wait();
+
+            return new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = getCredsTask.Result,
+                ApplicationName = "Google Cloud PowerShell",
+            };
+        }
+
+        /// <summary>
+        /// Prompt the user that they are about to perform the given action. Returns whether or not
+        /// they confirm it.
+        /// 
+        /// - Does not prompt if Force is set.
+        /// - Always prompts if WhatIF is set.
+        /// </summary>
+        protected bool ConfirmAction(bool force, string resource, string action)
+        {
+            // Always prompt if -WhatIf is added.
+            bool whatIfFlagValue = false;
+            if (base.MyInvocation.BoundParameters.ContainsKey("WhatIf"))
+            {
+                SwitchParameter whatIfFlag = (SwitchParameter)base.MyInvocation.BoundParameters["WhatIf"];
+                whatIfFlagValue = whatIfFlag.ToBool();
+            }
+            if (whatIfFlagValue)
+            {
+                return ShouldProcess(resource, action);
+            }
+
+            return force || ShouldProcess(resource, action);
+        }
+    }
+}
---
+++ Google.PowerShell/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿// Copyright 2015 Google Inc. All Rights Reserved.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PowerShell tools for the Google Cloud Platform")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Google Inc.")]
+[assembly: AssemblyProduct("PowerShell tools for the Google Cloud Platform")]
+[assembly: AssemblyCopyright("Copyright 2015 Google Inc.  All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Allow the unit tests assembly to view internal values.
+[assembly: InternalsVisibleTo("Google.PowerShell.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d338c5d3-5fc3-408f-9f7f-d53e7b213505")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
