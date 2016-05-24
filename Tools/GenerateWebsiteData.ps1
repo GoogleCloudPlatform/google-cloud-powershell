713c34014d7c3384bfb22f4536267d1f03a29240
Chris Smith <chrsmith@google.com>
2016-05-24 11:19:14 -0700

    Generate JSON file with all the cmdlet documentation

---
+++ Tools/GenerateWebsiteData.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module "C:\src\github.com\chrsmith\gcloud-powershell\Google.PowerShell\bin\Debug\Google.PowerShell.dll"
+
+$cmdlets = Get-Command -Module "Google.PowerShell"
+
+# Generate a single JSON file containing all the documentation for all the
+# cmdlets. Unfortunately we can't split these into multiple files because of
+# the way we generating web pages in Jekyll.
+$cmdletDocObjects = @()
+foreach ($cmdlet in $cmdlets) {
+    Write-Host "Building $($cmdlet.Name)..."
+    # Without Out-String the output of Get-Help will be a structured object,
+    # which would be useful for a custom renderer of help output.
+    $docText = Get-Help -Full $cmdlet.Name | Out-String
+    $cmdletDocObj = @{ "cmdletName"=$cmdlet.Name; "documentation"=$docText }
+    
+    $cmdletDocObjects += $cmdletDocObj
+}
+
+$cmdletDocObjects `
+| ConvertTo-Json -Depth 10 `
+| Out-File -FilePath "cmdlets.json" -Encoding "UTF8"
