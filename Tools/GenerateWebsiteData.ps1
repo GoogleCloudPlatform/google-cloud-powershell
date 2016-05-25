99b49ebe4117888230d05c5f0f42f3862390b865
Chris Smith <chrsmith@users.noreply.github.com>
2016-05-25 14:13:12 -0700

    Publish PowerShell cmdlet documentation to GitHub (#24)
    This PR adds the scripts necessary to extract all PowerShell documentation for cmdlets (e.g.
    `Get-Help New-GcsBucket`) and publish it to a [Jekyll](https://jekyllrb.com/)-powered website.
    
    Major aspects:
    
    - `Tools/GenerateWebsiteData.ps1`. This is the script that will run `Get-Help` on all the
        cmdlets and create a JSON file with the help contents.
    - `/website/`. This is a Jekyll-based website that will render the cmdlet data. (The current
        cmdlet documentation is checked in under `/website/_data/cmdlets.json`.)
    - 'website/_plugins/`. Contains a Jekyll-plugin that will generate HTML pages based on
        data. (Out of the box Jekyll does not have that capability.) Note the new `LICENSE.md`
        and `README.md` to properly cite the MIT-licensed plug-in. (I confirmed this is OK with
        Max Sills.)
    
    You can see the output of this website, which I have generated and published to GitHub at:
    http://googlecloudplatform.github.io/gcloud-powershell
    
    I have added instructions on how to build and redeploy the documentation website. If it is
    not clear please comment in the PR.

---
+++ Tools/GenerateWebsiteData.ps1
@@ -0,0 +1,35 @@
+﻿# Generates a JSON file containing the documentation information for all
+# PowerShell cmdlets. This can be passed to the documentation website and
+# -- by the magic of Jekyll -- be used to generate cmdlet-specific pages.
+#
+# After the script runs, copy `cmdlets.json` from the `Tools` folder into
+# `\website\_data\` and then rerun Jekyll.
+
+$binDirectory = Join-Path $PSScriptRoot "\..\Google.PowerShell\bin\"
+Import-Module "$binDirectory\Debug\Google.PowerShell.dll"
+
+$cmdlets = Get-Command -Module "Google.PowerShell"
+
+# Generate a single JSON file containing all the documentation for all the
+# cmdlets. Unfortunately we can't split these into multiple files because of
+# the way we generating web pages in Jekyll.
+$cmdletDocObjects = @()
+foreach ($cmdlet in $cmdlets) {
+    Write-Host "Building $($cmdlet.Name)..."
+    # Without Out-String the output of Get-Help will be a structured object,
+    # which would be useful for a custom renderer of help output.
+    $docText = Get-Help -Full $cmdlet.Name | Out-String
+    # TODO(chrsmith): Provide a "product" field, mapping "Gcs" to "Cloud Storage", etc.
+    $cmdletDocObj = @{ `
+        "cmdletName"=$cmdlet.Name; `
+        "resource"=$cmdlet.Name.Split("-")[1];
+        "documentation"=$docText `
+    }
+    
+    $cmdletDocObjects += $cmdletDocObj
+}
+
+Write-Host "Saving cmdlets.json"
+$cmdletDocObjects `
+| ConvertTo-Json -Depth 10 `
+| Out-File -FilePath "cmdlets.json" -Encoding "UTF8"
