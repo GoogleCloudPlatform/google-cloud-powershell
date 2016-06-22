48caeb77926b1a73a9f48d2bcab0e6bb3b79d19e
marceloyuela <moyuelab@andrew.cmu.edu>
2016-06-22 13:41:26 -0700

    First push. Basic SQL Cmdlet abstract class with a test cmdlet for the flags capability of Google Cloud SQL.

---
+++ Google.PowerShell/SQL/GcSqlCmdlet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.PowerShell.Common;
+using Google.Apis.SQLAdmin.v1beta4;
+
+
+
+namespace Google.PowerShell.SQL
+{
+    /// <summary>
+    /// Base class for Google Cloud SQL-based cmdlets. 
+    /// </summary>
+    public abstract class GcSqlCmdlet : GCloudCmdlet
+    {
+
+      //The service for the Google Cloud SQL API
+      public SQLAdminService Service { get; private set; }
+
+
+
+      public GcSqlCmdlet()
+      {
+          Service = new SQLAdminService(GetBaseClientServiceInitializer());
+      }
+    
+      //TODO: Do I need all the other functions that Jim put in his GceCmdlet?
+    }
+}
---
+++ Google.PowerShell/SQL/GcSqlFlagsCmdlet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.PowerShell.Common;
+using Google.Apis.SQLAdmin.v1beta4;
+using System.Management.Automation;
+using System.Diagnostics;
+using Google;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Services;
+using Google.Apis.SQLAdmin.v1beta4.Data;
+
+namespace Google.PowerShell.SQL
+{
+
+    /// <summary>
+    /// <para type="synopsis">
+    /// Lists all available database flags for Google Cloud SQL instances.
+    /// </para>
+    /// <para type="description">
+    /// Lists all available database flags for instances.
+    /// </para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GcSqlFlags")]
+    public class GcSqlFlagsCmdlet : GcSqlCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            FlagsResource.ListRequest flags = Service.Flags.List();
+            FlagsListResponse response = flags.Execute();
+            WriteObject("Items:");
+            WriteObject(response.Items);
+        }
+    }
+}
