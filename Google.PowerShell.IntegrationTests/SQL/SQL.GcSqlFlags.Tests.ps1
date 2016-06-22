2613be315cd39139c54ee4e1ae53e834450fcfda
marceloyuela <moyuelab@andrew.cmu.edu>
2016-06-22 16:20:46 -0700

    Added another test to verify at least two of the flags are actually being returned.

--- Google.PowerShell.IntegrationTests/SQL/SQL.GcSqlFlags.Tests.ps1
+++ Google.PowerShell.IntegrationTests/SQL/SQL.GcSqlFlags.Tests.ps1
@@ -5,11 +5,17 @@ $project = "gcloud-powershell-testing"
 
 Describe "Get-GcSqlFlags" {
 
-	It "should work" {
+	It "should have the correct number of flags" {
         $flags = Get-GcSqlFlags
 		#There are 38 flags available for Google Cloud SQL Instances.
         $flags.Length | Should be 38
 
     }
 
+	It "should have the correct flags" {
+		$flags = Get-GcSqlFlags
+		$flags."Name" -contains "log_output"
+		$flags."Name" -contains "group_concat_max_len"
+
+	}
 }
