1154f1113b6a6952aa3c3172daac55ff9f77c48b
marceloyuela <marceloyuela@google.com>
2016-06-22 17:43:30 -0700

    Personal sql branch (#43)
    Google Cloud SQL Flag List cmdlet done and tested.

---
+++ Google.PowerShell/SQL/GcSqlCmdlet.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2015-2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.PowerShell.Common;
+using Google.Apis.SQLAdmin.v1beta4;
+
+namespace Google.PowerShell.SQL
+{
+    /// <summary>
+    /// Base class for Google Cloud SQL-based cmdlets. 
+    /// </summary>
+    public abstract class GcSqlCmdlet : GCloudCmdlet
+    {
+        //The service for the Google Cloud SQL API
+        public SQLAdminService Service { get; private set; }
+
+        public GcSqlCmdlet()
+        {
+            Service = new SQLAdminService(GetBaseClientServiceInitializer());
+        }
+    }
+}
