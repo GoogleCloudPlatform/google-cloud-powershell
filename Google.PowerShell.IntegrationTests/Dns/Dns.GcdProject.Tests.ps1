c3611c2fdf6ee098102a3446a94097bdceb9b019
Emon Datta <edatta@google.com>
2016-06-24 12:46:08 -0700

    Added test conditions to check that project quota values are reasonable.

--- Google.PowerShell.IntegrationTests/Dns/Dns.GcdProject.Tests.ps1
+++ Google.PowerShell.IntegrationTests/Dns/Dns.GcdProject.Tests.ps1
@@ -14,12 +14,18 @@ Describe "Get-GcdProject" {
         { Get-GcdProject -Project "asdf" } | Should Throw "403"
     }
 
-    It "should work and retrieve project information" {
+    It "should work and retrieve valid project information" {
         $projectInfo = Get-GcdProject -Project $project
 
         $projectInfo.GetType().FullName | Should Match "Google.Apis.Dns.v1.Data.Project"
         $projectInfo.Id | Should Match $project
         $projectInfo.Kind | Should Match "dns#project"
-        $projectInfo.Quota | Should Match "Google.Apis.Dns.v1.Data.Quota"
+
+        $projectInfo.Quota.ManagedZones -ge 50 | Should Be $true
+        $projectInfo.Quota.ResourceRecordsPerRrset -ge 50 | Should Be $true
+        $projectInfo.Quota.RrsetAdditionsPerChange -ge 50 | Should Be $true
+        $projectInfo.Quota.RrsetDeletionsPerChange -ge 50 | Should Be $true
+        $projectInfo.Quota.RrsetsPerManagedZone -ge 9000 | Should Be $true
+        $projectInfo.Quota.TotalRrdataSizePerChange -ge 9000 | Should Be $true
     }
 }
