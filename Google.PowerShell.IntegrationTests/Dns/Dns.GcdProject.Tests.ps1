39ea1f326b5178c8a4926936c7b71290000d82d8
Emon Datta <edatta@google.com>
2016-06-23 18:57:21 -0700

    Added Get-GcdProject cmdlet and associated tests. Also set warning level to 4 in Google.PowerShell which then allowed me to remove/sort usings.

---
+++ Google.PowerShell.IntegrationTests/Dns/Dns.GcdProject.Tests.ps1
@@ -0,0 +1,25 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+$project = "gcloud-powershell-testing"
+
+Describe "Get-GcdProject" {
+
+    It "should fail to return representation of non-existent project" {
+        { Get-GcdProject -Project "project-no-exist" } | Should Throw "400"
+    }
+
+    It "should give access errors as appropriate" {
+        # Don't know who created the "asdf" project.
+        { Get-GcdProject -Project "asdf" } | Should Throw "403"
+    }
+
+    It "should work and retrieve project information" {
+        $projectInfo = Get-GcdProject -Project $project
+
+        $projectInfo.GetType().FullName | Should Match "Google.Apis.Dns.v1.Data.Project"
+        $projectInfo.Id | Should Match $project
+        $projectInfo.Kind | Should Match "dns#project"
+        $projectInfo.Quota | Should Match "Google.Apis.Dns.v1.Data.Quota"
+    }
+}
---
+++ Google.PowerShell/Dns/GcdProject.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using Google.Apis.Dns.v1;
+using Google.Apis.Dns.v1.Data;
+using Google.PowerShell.Common;
+using System.Management.Automation;
+
+namespace Google.PowerShell.Dns
+{
+    /// <summary>
+    /// <para type="synopsis">
+    /// Fetch the representation of an existing project.
+    /// </para>
+    /// <para type="description">
+    /// Returns the Project resource object.
+    /// </para>
+    /// <para type="description">
+    /// If a project is specified, will instead return the representation of that project. 
+    /// </para>
+    /// <example>
+    ///   <para>Get the representation of the project “testing”</para>
+    ///   <para><code>Get-GcdProject -Project "testing" </code></para>
+    /// </example>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GcdProject")]
+    public class GetGcdProjectCmdlet : GcdCmdlet
+    {
+        /// <summary>
+        /// <para type="description">
+        /// Get the project to return a representation of.
+        /// </para>
+        /// </summary>
+        [Parameter]
+        [ConfigPropertyName(CloudSdkSettings.CommonProperties.Project)]
+        public string Project { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            ProjectsResource.GetRequest projectRequest = Service.Projects.Get(Project);
+            Project projectResponse = projectRequest.Execute();
+            WriteObject(projectResponse);
+        }
+    }
+}
