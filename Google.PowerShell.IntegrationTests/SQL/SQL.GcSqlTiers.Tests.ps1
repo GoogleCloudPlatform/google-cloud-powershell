c000c52f9ecd075368aed1c83b30f8f6e1c28cd4
marceloyuela <moyuelab@andrew.cmu.edu>
2016-06-24 13:45:34 -0700

    The cmdlet and corresponding test for the only API usage related to tiers. Also the updated project build.

---
+++ Google.PowerShell.IntegrationTests/SQL/SQL.GcSqlTiers.Tests.ps1
@@ -0,0 +1,19 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+$project = "gcloud-powershell-testing"
+
+Describe "Get-GcSqlFlags" {
+
+    It "should have the correct number of tiers" {
+        $tiers = Get-GcSqlTiers -Project $project
+        #There are 18 tiers available for our Google Cloud SQL Project.
+        $tiers."TierValue".Length | Should be 18
+    }
+
+    It "should have the correct flags" {
+        $tiers = Get-GcSqlTiers -Project $project
+        $tiers."TierValue" -contains "D0"
+        $tiers."TierValue" -contains "D4"
+    }
+}
---
+++ Google.PowerShell/SQL/GcSqlTiersCmdlet.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2015-2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.SQLAdmin.v1beta4.Data;
+using Google.Apis.SQLAdmin.v1beta4;
+using System.Management.Automation;
+
+namespace Google.PowerShell.SQL
+{
+
+    /// <summary>
+    /// <para type="synopsis">
+    /// Lists all available service tiers for Google Cloud SQL, for example D1, D2. 
+    /// </para>
+    /// <para type="description">
+    /// Lists all available service tiers for Google Cloud SQL, for example D1, D2. Pricing information is available online.
+    /// </para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GcSqlTiers")]
+    public class GcSqlTiersCmdlet : GcSqlCmdlet
+    {
+        /// <summary>
+        /// <para type="description">
+        /// Project ID of the project for which to list tiers.
+        /// </para>
+        /// </summary>
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Project { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            TiersResource resource = new TiersResource(Service);
+            TiersResource.ListRequest request = resource.List(Project);
+            TiersListResponse tiers = request.Execute();
+            WriteObject(tiers.Items);
+        }
+    }
+}
