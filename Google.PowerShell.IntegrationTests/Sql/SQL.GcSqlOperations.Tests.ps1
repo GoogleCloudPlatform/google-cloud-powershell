2f2f60bea20c750ed2202bb9d0b2dcd9b0eaf1b9
marceloyuela <marceloyuela@google.com>
2016-06-28 13:46:09 -0700

    Sql operations cmdlet  completed(#54)
    * Adding the csproj and packe files I forgot to commit yesterday- sorry!
    
    * The cmdlet and corresponding test for the only API usage related to tiers. Also the updated project build.
    
    * I didn't realize I had to push those too
    
    * Deleted those backup files and fixed the quotations.
    
    * Should have removed merge artifacts.
    
    * Fixed various things in notation and correctness.
    
    * Committing so I can merge
    
    * Fixed merge issue. Woops.
    
    * Knocked out the two cmdlets and their tests for Operations.
    
    * Forgot to push the integration tests project file in the last commit. Woops.
    
    * Merged together those two cmdlets.
    
    * Fixed that issue with naming.

---
+++ Google.PowerShell.IntegrationTests/Sql/SQL.GcSqlOperations.Tests.ps1
@@ -0,0 +1,32 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+$project = "gcloud-powershell-testing"
+
+Describe "Get-GcSqlOperations" {
+
+    It "should get a reasonable response" {
+        $operations = Get-GcSqlOperation -Project $project -Instance "test-db"
+        ($operations.operationType -contains "Create") | Should Be true
+    }
+
+	It "should get a reasonable response from a given query" {
+        $operation = Get-GcSqlOperation -Project $project -Name "515a9bb2-fcad-4c62-a8c5-3cfb7cbabfa2"
+        $operation.operationType | Should Be "Create"
+    }
+
+    It "should compound with Get-GcSqlOperations" {
+        $operations = Get-GcSqlOperation -Project $project -Instance "test-db"
+        $firstOperation = $operations | Select-Object -first 1
+        $operationName = $firstOperation.name
+        $operation = Get-GcSqlOperation -Project $project -Name $operationName
+        $operation.name | Should Be $firstOperation.name
+        $operation.operationType | Should Be $firstOperation.operationType
+    }
+
+    It "shouldn't require Project to be passed in to work" {
+        $operation = Get-GcSqlOperation -Name "515a9bb2-fcad-4c62-a8c5-3cfb7cbabfa2"
+        $operation.operationType | Should Be "Create"
+    }
+}
+
