be989cc523b47d30361e102ce764f260bed61e99
marceloyuela <moyuelab@andrew.cmu.edu>
2016-06-29 15:39:04 -0700

    Did the cmdlet and tests for getting instances while the backruprun one was in pull request.

---
+++ Google.PowerShell/Sql/GcSqlInstanceCmdlets.cs
@@ -0,0 +1,84 @@
+﻿// Copyright 2015-2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using Google.Apis.SQLAdmin.v1beta4;
+using Google.Apis.SQLAdmin.v1beta4.Data;
+using System.Management.Automation;
+using Google.PowerShell.Common;
+using System.Collections.Generic;
+
+namespace Google.PowerShell.Sql
+{
+    /// <summary>
+    /// <para type="synopsis">
+    /// Retrieves a resource containing information about a Cloud SQL instance, or lists all instances in a project.
+    /// </para>
+    /// <para type="description">
+    /// Retrieves a resource containing the information for the specified Cloud SQL instance, 
+    /// or lists all instances in a project.
+    /// This is determined by if Instance is specified or not.
+    /// </para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GcSqlInstance")]
+    public class GetGcSqlInstanceCmdlet : GcSqlCmdlet
+    {
+        internal class ParameterSetNames
+        {
+            public const string GetSingle = "Single";
+            public const string GetList = "List";
+        }
+
+        /// <summary>
+        /// <para type="description">
+        /// Project name of the project that contains instance(s).
+        /// Defaults to the cloud sdk config for properties if not specified.
+        /// </para>
+        /// </summary>
+        [Parameter(ParameterSetName = ParameterSetNames.GetSingle)]
+        [Parameter(ParameterSetName = ParameterSetNames.GetList)]
+        [ConfigPropertyName(CloudSdkSettings.CommonProperties.Project)]
+        public string Project { get; set; }
+
+        /// <summary>
+        /// <para type="description">
+        /// Cloud SQL instance name. 
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = ParameterSetNames.GetList)]
+        public string Instance { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            if (Instance != null) {
+                InstancesResource.GetRequest request = Service.Instances.Get(Project, Instance);
+                DatabaseInstance result = request.Execute();
+                WriteObject(result);
+            }
+            else 
+            {
+                IEnumerable<DatabaseInstance> results = GetAllSqlInstances();
+                WriteObject(results, true);
+            }
+        }
+
+        private IEnumerable<DatabaseInstance> GetAllSqlInstances()
+        {
+            InstancesResource.ListRequest request = Service.Instances.List(Project);
+            do
+            {
+                InstancesListResponse aggList = request.Execute();
+                IList<DatabaseInstance> instanceList = aggList.Items;
+                if (instanceList == null)
+                {
+                    yield break;
+                }
+                foreach (DatabaseInstance instance in instanceList)
+                {
+                    yield return instance;
+                }
+                request.PageToken = aggList.NextPageToken;
+            }
+            while (request.PageToken != null);
+        }
+    }
+}
