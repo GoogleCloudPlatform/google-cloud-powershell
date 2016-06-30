1c2507911d100435e5094bd8e631402d348893ce
Emon Datta <edatta@google.com>
2016-06-30 15:25:42 -0700

    Clean up Pester tests. Created a GcdCmdlets.ps1 file that sets up useful variables and functions used in the Dns cmdlet tests. Also cleaned up some syntax and removed extraneous tests.

--- Google.PowerShell.IntegrationTests/Dns/Dns.GcdProject.Tests.ps1
+++ Google.PowerShell.IntegrationTests/Dns/Dns.GcdProject.Tests.ps1
@@ -1,25 +1,25 @@
-﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
-Install-GcloudCmdlets
-
-$project = "gcloud-powershell-testing"
+﻿. $PSScriptRoot\..\Dns\GcdCmdlets.ps1
 
 Describe "Get-GcdProject" {
+    BeforeAll {
+        Remove-AllManagedZone($project)
+    }
 
     It "should fail to return representation of non-existent project" {
-        { Get-GcdProject -Project "project-no-exist" } | Should Throw "400"
+        { Get-GcdProject -Project $nonExistProject } | Should Throw "400"
     }
 
     It "should give access errors as appropriate" {
         # Don't know who created the "asdf" project.
-        { Get-GcdProject -Project "asdf" } | Should Throw "403"
+        { Get-GcdProject -Project $accessErrProject } | Should Throw "403"
     }
 
     It "should work and retrieve valid project information" {
         $projectInfo = Get-GcdProject -Project $project
 
-        $projectInfo.GetType().FullName | Should Match "Google.Apis.Dns.v1.Data.Project"
+        $projectInfo.GetType().FullName | Should Match $projectType
         $projectInfo.Id | Should Match $project
-        $projectInfo.Kind | Should Match "dns#project"
+        $projectInfo.Kind | Should Match $projectKind
 
         $projectInfo.Quota.ManagedZones -ge 50 | Should Be $true
         $projectInfo.Quota.ResourceRecordsPerRrset -ge 50 | Should Be $true
---
+++ Google.PowerShell.IntegrationTests/Dns/GcdCmdlets.ps1
@@ -0,0 +1,62 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+# Define variables that will be used in testing
+$project = "gcloud-powershell-testing"
+
+$nonExistProject = "project-no-exist"
+$nonExistManagedZone = "zone-no-exist"
+$accessErrProject = "asdf"
+
+$changeType = "Google.Apis.Dns.v1.Data.Change"
+$managedZoneType = "Google.Apis.Dns.v1.Data.ManagedZone"
+$projectType = "Google.Apis.Dns.v1.Data.Project"
+$rrsetType = "Google.Apis.Dns.v1.Data.ResourceRecordSet"
+
+$changeKind = "dns#change"
+$managedZoneKind = "dns#managedZone"
+$projectKind = "dns#project"
+$rrsetKind = "dns#resourceRecordSet"
+
+$testZone1 = "test1"
+$testZone2 = "test2"
+
+$testDescrip1 = "test1 description"
+$testDescrip2 = "test2 description"
+
+$dnsName1 = "gcloudexample1.com."
+$dnsName1_1 = "a.gcloudexample1.com."
+$dnsName1_2 = "b.gcloudexample1.com."
+$dnsName1_3 = "c.gcloudexample1.com."
+$dnsName2 = "gcloudexample2.com."
+
+$rrdata1 = "7.5.7.8"
+$rrdata1_1 = "7.5.6.8"
+$rrdata2 = "2001:0db8:85a3:0:0:8a2e:0370:7334"
+
+$ttl1 = 300
+$ttlDefault = 3600
+
+$testRrset1 = New-GcdResourceRecordSet -Name $dnsName1 -Rrdata $rrdata1 -Type "A" -Ttl $ttl1
+
+$transactionFile = "transaction.yaml"
+
+$Err_NeedChangeContent = "Must specify at least 1 non-null, non-empty value for Add or Remove."
+$Err_ProjectZonesNotDeleted = "All ManagedZones in the specified project have not been deleted."
+
+# Define functions that will be used in testing
+
+# Force remove all existing ManagedZones, including non-empty ones
+# TODO(edatta): Simplify once Remove-GcdManagedZone cmdlet is done.
+function Remove-AllManagedZone($projectName) {
+    $preExistingZones = Get-GcdManagedZone -Project $project
+
+    ForEach ($zoneObject in $preExistingZones) {
+        $zoneName = $zoneObject.Name
+
+        New-Item empty-file -Force
+        gcloud dns record-sets import --zone=$zoneName --delete-all-existing empty-file
+        Remove-Item empty-file -Force
+        gcloud dns managed-zones delete $zoneName --project=$project
+    }
+}
