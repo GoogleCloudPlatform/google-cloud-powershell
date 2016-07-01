13a86e640bc208612833b5e5eddab7e20bf7be4c
Jim Przybylinski <jimwp@google.com>
2016-06-30 17:47:03 -0700

    Add snapshot cmdlets.

---
+++ Google.PowerShell.IntegrationTests/Compute/Compute.GceSnapshot.Tests.ps1
@@ -0,0 +1,129 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+$project, $zone, $oldActiveConfig, $configName = Set-GCloudConfig
+
+$r = Get-Random
+
+$diskName = "test-image-disk-$r"
+$disk = New-GceDisk -DiskName $diskName -SizeGb 1
+
+Describe "Add-GceSnapshot" {
+    $snapshotName = "test-add-snapshot-$r"
+
+    It "should fail on wrong project" {
+        { Add-GceSnapshot $diskName -Project "asdf" } | Should Throw 403
+    }
+
+    It "should fail on non-existant disk" {
+        { Add-GceSnapshot "not-a-real-disk" } | Should Throw 404
+    }
+
+    It "should work with disk name" {
+        $snapshot = Add-GceSnapshot $diskName -Name $snapshotName -Description "for testing $r"
+        ($snapshot | Get-Member).TypeName | Should Be Google.Apis.Compute.v1.Data.Snapshot
+        $snapshot.Name | Should Be $snapshotName
+        $snapshot.Descripton | Should Be "for testing $r"
+        { Get-GceSnapshot $snapshot.Name } | Should Not Throw
+    }
+
+    It "should work with disk name on pipeline" {
+        $snapshot = $diskName | Add-GceSnapshot 
+        ($snapshot | Get-Member).TypeName | Should Be Google.Apis.Compute.v1.Data.Snapshot
+        $snapshot.Name | Should Match "$diskName-"
+        { Get-GceSnapshot $snapshot.Name } | Should Not Throw
+    }
+
+    It "should work with disk object" {
+        $snapshot = Add-GceSnapshot $disk
+        $snapshot | Should Not BeNullOrEmpty
+        { Get-GceSnapshot $snapshot.Name } | Should Not Throw
+    }
+
+    It "should work with disk object on pipeline" {
+        $snapshot = $disk | Add-GceSnapshot
+        $snapshot | Should Not BeNullOrEmpty
+        { Get-GceSnapshot $snapshot.Name } | Should Not Throw
+    }
+
+    Get-GceSnapshot | Remove-GceSnapshot
+}
+
+Describe "Get-GceSnapshot" {
+    $snapshotName = "test-get-snapshot-$r"
+    $snapshotName2 = "test-get-snapshot2-$r"
+
+    It "should fail on wrong project" {
+        { Get-GceSnapshot $snapshotName -Project "asdf" } | Should Throw 403
+    }
+
+    It "should fail on non-existant snapshot" {
+        { Get-GceSnapshot "not-a-real-snapshot" } | Should Throw 404
+    }
+
+    It "should work with no snapshots" {
+        $snapshots = Get-GceSnapshot
+        $snapshots | Should BeNullOrEmpty
+    }
+
+    $disk | Add-GceSnapshot -Name $snapshotName
+    $disk | Add-GceSnapshot -Name $snapshotName2
+
+    It "should get all project snapshots" {
+        $snapshots = Get-GceSnapshot
+        ($snapshots | Get-Member).TypeName | Should Be Google.Apis.Compute.v1.Data.Snapshot
+        $snapshots.Count | Should Be 2
+        ($snapshosts.Name -eq $snapshotName).Count | Should Be 1
+        ($snapshosts.Name -eq $snapshotName2).Count | Should Be 1
+    }
+
+    It "should get snapshot by name" {
+        $snapshot = Get-GceSnapshot $snapshotName
+        $snapshot.Count | Should Be 1
+        $shanpshot.Name | Should Be $snapshotName
+    }
+
+    Get-GceSnapshot | Remove-GceSnapshot
+}
+
+Describe "Remove-GceSnapshot" {
+    $snapshotName = "test-get-snapshot-$r"
+
+    It "should fail on wrong project" {
+        { Remove-GceSnapshot $snapshotName -Project "asdf" } | Should Throw 403
+    }
+
+    It "should fail on non-existant snapshot" {
+        { Remove-GceSnapshot "not-a-real-snapshot" } | Should Throw 404
+    }
+
+    Context "remove successes" {
+
+        BeforeEach {
+            $disk | Add-GceSnapshot -Name $snapshotName
+        }
+
+        It "should work by name" {
+            Remove-GceSnapshot $snapshotName
+            { Get-GceSnapshot $snapshotName } | Should Throw 404
+        }
+
+        It "should work by name with pipeline" {
+            $snapshotName | Remove-GceSnapshot 
+            { Get-GceSnapshot $snapshotName } | Should Throw 404
+        }
+
+        It "should work by object" {
+            $snapshot = Get-GceSnapshot $snapshotName
+            Remove-GceSnapshot $snapshot
+            { Get-GceSnapshot $snapshotName } | Should Throw 404
+        }
+
+        It "should work by object with pipeline" {
+            Get-GceSnapshot $snapshotName | Remove-GceSnapshot
+            { Get-GceSnapshot $snapshotName } | Should Throw 404
+        }
+    }
+}
+
+Reset-GCloudConfig $oldActiveConfig $configName
