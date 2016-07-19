28ea2c2af2b9f4db32233e077afe4037254cd359
Emon Datta <edatta@google.com>
2016-07-19 14:05:19 -0700

    Add Promote-GcSqlReplica cmdlet and associated tests.

--- Google.PowerShell.IntegrationTests/SQL/SQL.GcSqlInstances.Tests.ps1
+++ Google.PowerShell.IntegrationTests/SQL/SQL.GcSqlInstances.Tests.ps1
@@ -424,4 +424,71 @@ Describe "Stop-GcSqlReplica" {
      }
 }
 
+Describe "Promote-GcSqlReplica" {
+    # For these tests, test-db2 was used because an instance must have a database and a binarylog for it to be 
+    # replicated. This kind of instance cannot be easily/quickly instantiated like those in other tests.
+    $masterInstance = "test-db2"
+    $2ndGenTier = "db-n1-standard-1"
+
+    It "should work and promote a test replica" {
+        # A random number is used to avoid collisions with the speed of creating and deleting instances/replicas.
+        $r = Get-Random
+        $replica = "test-repl$r"
+        gcloud sql instances create $replica --master-instance-name $masterInstance --tier $2ndGenTier --replication SYNCHRONOUS --quiet 2>$null
+        Promote-GcSqlReplica -Replica $replica
+
+        $operations = Get-GcSqlOperation -Instance $replica
+        $operations.Count | Should Be 2
+        $operations[0].OperationType | Should Match "PROMOTE_REPLICA"
+        $operations[0].Status | Should Match "DONE"
+        $operations[0].Error | Should Match ""
+        $operations[1].OperationType | Should Match "CREATE_REPLICA"
+
+        gcloud sql instances delete $replica --quiet 2>$null
+    }
+
+     It "should work and promote a pipelined replica (replica and default projects same)" {
+         # A random number is used to avoid collisions with the speed of creating and deleting instances/replicas.
+        $r = Get-Random
+        $replica = "test-repl$r"
+        gcloud sql instances create $replica --master-instance-name $masterInstance --tier $2ndGenTier --replication SYNCHRONOUS --quiet 2>$null
+        Get-GcSqlInstance -Name $replica | Promote-GcSqlReplica
+
+        $operations = Get-GcSqlOperation -Instance $replica
+        $operations.Count | Should Be 2
+        $operations[0].OperationType | Should Match "PROMOTE_REPLICA"
+        $operations[0].Status | Should Match "DONE"
+        $operations[0].Error | Should Match ""
+        $operations[1].OperationType | Should Match "CREATE_REPLICA"
+
+        gcloud sql instances delete $replica --quiet 2>$null
+     }
+
+    It "should work and promote a pipelined replica (replica and default projects differ)" {
+        $nonDefaultProject = "asdf"
+        $defaultProject = "gcloud-powershell-testing"
+
+        # Set gcloud config to a non-default project (not gcloud-powershell-testing)
+        gcloud config set project $nonDefaultProject
+
+         # A random number is used to avoid collisions with the speed of creating and deleting instances/replicas.
+        $r = Get-Random
+        $replica = "test-repl$r"
+        gcloud sql instances create $replica --master-instance-name $masterInstance --tier $2ndGenTier --replication SYNCHRONOUS --project $defaultProject --quiet 2>$null
+        Get-GcSqlInstance -Project $defaultProject -Name $replica | Promote-GcSqlReplica
+
+        $operations = Get-GcSqlOperation -Project $defaultProject -Instance $replica
+        $operations.Count | Should Be 2
+        $operations[0].OperationType | Should Match "PROMOTE_REPLICA"
+        $operations[0].Status | Should Match "DONE"
+        $operations[0].Error | Should Match ""
+        $operations[1].OperationType | Should Match "CREATE_REPLICA"
+
+        gcloud sql instances delete $replica --project $defaultProject --quiet 2>$null
+
+        # Reset gcloud config back to default project (gcloud-powershell-testing)
+        gcloud config set project $defaultProject
+     }
+}
+
 Reset-GCloudConfig $oldActiveConfig $configName
