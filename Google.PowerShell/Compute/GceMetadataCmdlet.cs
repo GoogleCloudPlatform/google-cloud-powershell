9fa7f6807625cf320409248f12a8fd4dd68905be
Jim Przybylinski <jimwp@google.com>
2016-08-09 14:01:42 -0700

    Add a few more pieces of functionality. Fix a bug.

--- Google.PowerShell/Compute/GceMetadataCmdlet.cs
+++ Google.PowerShell/Compute/GceMetadataCmdlet.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License Version 2.0.
 
 using Google.PowerShell.Common;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,23 +10,82 @@ using System.Net;
 
 namespace Google.PowerShell.ComputeEngine
 {
+    /// <summary>
+    /// <para type="synopsis">
+    /// Gets the current metadata from the metadata server.
+    /// </para>
+    /// <para type="description">
+    /// Gets the current metadata from the metadata server. Get-GceMetadata can only be called from a Google
+    /// Compute Engine VM instance. Calls from any other machine will fail.
+    /// </para>
+    /// <example>
+    /// <code>PS C:\> $allMetadata = Get-GceMetadata | ConvertFrom-Json</code>
+    /// <para>Gets all of the metadata and converts it from the given json to a PSCustomObject.</para>
+    /// </example>
+    /// <example>
+    /// <code>PS C:\> $hostName = Get-GceMetadata -Path "instance/hostname" -NotRecursive</code>
+    /// <para>Gets the hostname of the instance.</para>
+    /// </example>
+    /// <example>
+    /// <code>
+    ///     PS C:\> $newTags, $newEtag = Get-GceMetadata -Path "instance/tags" -AppendEtag -WaitUpdate `
+    ///         -LastETag $oldETag
+    /// </code>
+    /// </example>
+    /// </summary>
     [Cmdlet(VerbsCommon.Get, "GceMetadata")]
+    [OutputType(typeof(string))]
     public class GetGceMetadataCmdlet : GCloudCmdlet
     {
+        /// <summary>
+        /// <para type="description">
+        /// The path to the specific metadata you wish to get e.g. "instance/tags", "instance/attributes",
+        /// "project/attributes/sshKeys".
+        /// </para>
+        /// </summary>
         [Parameter]
         public string Path { get; set; } = "";
 
+        /// <summary>
+        /// <para type="description">
+        /// If set, will only get the direct children of the current path.
+        /// </para>
+        /// </summary>
+        [Parameter]
+        public SwitchParameter NotRecursive { get; set; }
+
+        /// <summary>
+        /// <para type="description">
+        /// When set, the value of the respone etag will be appended to the output pipeline after the content.
+        /// </para>
+        /// </summary>
+        [Parameter]
+        public SwitchParameter AppendETag { get; set; }
+
+        /// <summary>
+        /// <para type="description">
+        /// If true, the query will wait for the metadata to update.
+        /// </para>
+        /// </summary>
         [Parameter]
         public SwitchParameter WaitUpdate { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// The last etag known. Used in conjunction with -WaitUpdate. If the last etag does not match the
+        /// current etag of the metadata server, it will return immediatly.
+        /// </para>
+        /// </summary>
         [Parameter]
-        public string ETag { get; set; }
+        public string LastETag { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// Used in conjunction with -WaitUpdate. The amout of time to wait before returning.
+        /// </para>
+        /// </summary>
         [Parameter]
         public TimeSpan? Timeout { get; set; }
-
-        [Parameter]
-        public SwitchParameter NotRecursive { get; set; }
 
         protected override void ProcessRecord()
         {
@@ -40,25 +98,33 @@ namespace Google.PowerShell.ComputeEngine
             if (WaitUpdate)
             {
                 queryParameters.Add("wait_for_change=true");
-                if (ETag != null)
+                if (LastETag != null)
                 {
-                    queryParameters.Add($"last_etag={ETag}");
+                    queryParameters.Add($"last_etag={LastETag}");
                 }
                 if (Timeout != null)
                 {
-                    queryParameters.Add($"timeout_set={(int)Timeout.Value.TotalSeconds}");
+                    queryParameters.Add($"timeout_sec={(int)Timeout.Value.TotalSeconds}");
                 }
             }
 
             string query = string.Join("&", queryParameters);
             HttpWebRequest request = WebRequest.CreateHttp($"{basePath}{Path}?{query}");
-            request.Headers.Add("Metadata-Flavor: Google");
+            request.Headers.Add("Metadata-Flavor:Google");
+            if (WaitUpdate)
+            {
+                request.Timeout = -1;
+            }
 
             using (WebResponse response = request.GetResponse())
             using (Stream responseStream = response.GetResponseStream())
             using (StreamReader streamReader = new StreamReader(responseStream))
             {
-                WriteObject(JsonConvert.DeserializeObject(streamReader.ReadToEnd()));
+                WriteObject(streamReader.ReadToEnd());
+                if (AppendETag)
+                {
+                    WriteObject(response.Headers["ETag"]);
+                }
             }
         }
     }
