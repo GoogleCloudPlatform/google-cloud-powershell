a7e74156b4cf47443a4fb05a59273f5a3d85ffcb
marceloyuela <marceloyuela@google.com>
2016-08-22 17:08:12 -0700

    Added prevention of invalid route parameters.

--- website_v2/gcloud-powershell-website-v2/www/static/app/referenceController.js
+++ website_v2/gcloud-powershell-website-v2/www/static/app/referenceController.js
@@ -3,5 +3,20 @@
 /** The controller that exposes route parameters to the templates. **/
 app.controller('ReferenceController', ['$scope', '$routeParams',
     function($scope, $routeParams) {
+        var prodInfo = $scope.productInfo;
+        /** We have to make sure no invalid routes were passed in **/
+        if (Object.keys($routeParams).length === 2) {
+            if (!($routeParams.product in prodInfo)
+                || !($routeParams.cmdlet in prodInfo[$routeParams.product])) {
+                    console.error("Invalid Product or Cmdlet");
+                    return;
+            }
+        }
+        else if (Object.keys($routeParams).length === 1) {
+            if (!($routeParams.product in $scope.productInfo)) {
+                console.error("Invalid Product or Cmdlet");
+                return;
+            }
+        }
         this.params = $routeParams;
 }]);
