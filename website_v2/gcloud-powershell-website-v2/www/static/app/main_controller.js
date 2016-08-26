fec6d5b9a1563f8b84829e189030684828bde502
marceloyuela <marceloyuela@google.com>
2016-08-26 11:21:06 -0700

    Fixed some prevaling style issues according to the google style guide.

--- website_v2/gcloud-powershell-website-v2/www/static/app/main_controller.js
+++ website_v2/gcloud-powershell-website-v2/www/static/app/main_controller.js
@@ -2,20 +2,19 @@ var app = angular.module('powershellSite');
 
 app.controller('MainController',
     function($scope, $http) {
-    $scope.productInfo = {};
-    try {
-        /**
+      $scope.productInfo = {};
+      try {
+        /*
          * We have to chain promises in order to supply the data
          * without causing a race condition.
-        **/
+         */
         $scope.res = $http.get('static/_data/cmdletsFull.json')
-        .then(function(res) {
-            /** We store the json info on the scope so everything has access **/
-            $scope.productInfo = res.data;
-       });
-    }
-    catch (err) {
+            .then(function(res) {
+              /* We store the json info on the scope so everything has access */
+              $scope.productInfo = res.data;
+        });
+      } catch (err) {
         $scope.productInfo = null;
         console.error(err);
-    }
+      }
 });
---
+++ website_v2/gcloud-powershell-website-v2/www/static/app/table_controller.js
@@ -0,0 +1,36 @@
+﻿var app = angular.module('powershellSite');
+
+/* The controller for the table of contents. */
+app.controller('TableController', function($scope, $attrs) {
+  /* Whether or not a product's information is expanded. */
+  this.expanded = false;
+
+  /** What the current active product is. */
+  this.activeProduct = '';
+
+  /*
+   * onProductClick is used when a product is clicked.
+   * 'productName' is the product clicked.
+   */
+  this.onProductClick = function(productName) {
+    /* It either closes the current expansion and shows the home page */
+    if (this.activeProduct === productName) {
+      this.activeProduct = '';
+    }
+    /*
+     * Or it sets the information to be the information screen for
+     * the applicable product.
+     */
+    else {
+      this.activeProduct = productName;
+    }
+  };
+
+  /*
+   * isExpanded just tells whether or not a product is expanded
+   * 'productName' is the product we are checking.
+   */
+  this.isExpanded = function(productName) {
+    return (this.activeProduct === productName);
+  };
+});
