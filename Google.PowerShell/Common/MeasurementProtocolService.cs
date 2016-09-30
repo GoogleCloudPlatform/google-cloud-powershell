5ea449dbcf793ef6fd7fe28a1274fa51cadec1d1
Chris Smith <chrsmith@google.com>
2016-09-30 15:39:57 -0700

    Upgrade to newer Google Analytics wrapper

--- Google.PowerShell/Common/MeasurementProtocolService.cs
+++ Google.PowerShell/Common/MeasurementProtocolService.cs
@@ -1,144 +1,19 @@
 ﻿// Copyright 2015-2016 Google Inc. All Rights Reserved.
 // Licensed under the Apache License Version 2.0.
 
+using GoogleAnalyticsUtils;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Web;
 
 namespace Google.PowerShell.Common
 {
-    /// <summary>
-    /// Client for the the Google Analytics Measurement Protocol service, which makes
-    /// HTTP requests to publish data to a Google Analytics account. This is used to track
-    /// usage of PowerShell cmdlets.
-    /// 
-    /// For more information, see:
-    /// https://developers.google.com/analytics/devguides/collection/protocol/v1/
-    ///
-    /// The following convention is assumed to be used:
-    /// - "category" is fixed to be "PowerShell"
-    /// - "action" is the name of the cmdlet
-    /// - "label" is the name of the parameter set
-    /// - "value" will be null if the cmdlet was successful, otherwise non-zero.
-    ///
-    /// This class is thread hostile. You have been warned.
-    /// </summary>
-    public class MeasurementProtocolService
-    {
-        // TODO(chrsmith): After the code has been submitted and had some bake time,
-        // change this to the prod Cloud SDK analytics property ID ("UA-36037335-2").
-        private const string TestWebPropertyId = "UA-80810157-1";
-
-        // Static constructor initializes the default values.
-        static MeasurementProtocolService()
-        {
-            // Consumers should set a more appropriate ID once known. e.g. reading the
-            // Cloud SDK's settings file.
-            SetClientId(Guid.NewGuid().ToString());
-        }
-
-        /// <summary>
-        /// Google Analytics web property ID to associate the data with.
-        /// </summary>
-        public static string WebPropertyId { get; } = TestWebPropertyId;
-
-        /// <summary>
-        /// Anonymous client ID for the source of the event.
-        /// </summary>
-        public static string ClientId { get; protected set; }
-
-        /// <summary>
-        /// Sets the client ID used when reporting telemetry.
-        /// </summary>
-        public static void SetClientId(string clientId)
-        {
-            AssertArgumentNotNullOrEmpty(nameof(clientId), clientId);
-            ClientId = clientId.Trim();
-        }
-
-        /// <summary>
-        /// Generates the HTTP request object used for sending telemetry data.
-        /// </summary>
-        public static HttpWebRequest GenerateRequest(string category, string action, string label, int? value = null)
-        {
-            AssertArgumentNotNullOrEmpty(nameof(category), category);
-            AssertArgumentNotNullOrEmpty(nameof(action), action);
-            AssertArgumentNotNullOrEmpty(nameof(label), label);
-
-            // If you need help debugging the request, see the Validation Server at
-            // /debug/collect and then inspect the response.
-            var request = (HttpWebRequest)WebRequest.Create("https://www.google-analytics.com/collect");
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.KeepAlive = false;
-
-            // Data we will send along with the web request. Later baked into the HTTP
-            // request's payload.
-            var payloadData = new Dictionary<string, string> {
-                { "v", "1" },
-                { "tid", WebPropertyId },
-                { "cid", ClientId },
-                { "t", "event" },
-                { "ec", category },
-                { "ea", action },
-                { "el", label },
-            };
-            if (value.HasValue)
-            {
-                payloadData.Add("ev", value.ToString());
-            }
-
-            // Convert the URL parameters into a single payload.
-            string postDataString = String.Join(
-                "&",
-                payloadData.Select(kvp => $"{kvp.Key}={HttpUtility.UrlEncode(kvp.Value)}"));
-
-            var encoding = new UTF8Encoding(false /* no BOM */);
-            request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
-            using (var writer = new StreamWriter(request.GetRequestStream(), encoding))
-            {
-                writer.Write(postDataString);
-            }
-
-            return request;
-        }
-
-        /// <summary>
-        /// Sends the web request, effectively sending the telemetry data to Google Analytics.
-        /// </summary>
-        public static void IssueRequest(HttpWebRequest request)
-        {
-            try
-            {
-                using (var webResponse = (HttpWebResponse)request.GetResponse())
-                {
-                    // If usage data seems too low, consider debugging this line
-                    // and checking webResponse.StatusCode.
-                }
-            }
-            catch (Exception ex)
-            {
-                // Silently ignore it. Even if the request was malformed Google Analytics will
-                // return a 200. So I assume this would only happen in the event of some transient
-                // network failure, e.g. there is no internet connection.
-                Debug.WriteLine("Error issuing Analytics request: {0}", ex.Message);
-            }
-        }
-
-        private static void AssertArgumentNotNullOrEmpty(string argumentName, string argumentValue)
-        {
-            if (string.IsNullOrEmpty(argumentValue))
-            {
-                throw new ArgumentNullException(argumentName);
-            }
-        }
-    }
-
     /// <summary>
     /// Interface for interacting with the Measurement Protocol service. See concrete
     /// implementations for details.
@@ -248,29 +123,75 @@ namespace Google.PowerShell.Common
     }
 
     /// <summary>
+    /// Type of analytic event.
+    /// </summary>
+    public enum AnalyticsEventCategory
+    {
+        CmdletInvocation,
+        Provider
+    }
+
+    /// <summary>
     /// Reports PowerShell cmdlet results to Google Analytics.
     /// </summary>
     public class GoogleAnalyticsCmdletReporter : IReportCmdletResults
     {
-        public GoogleAnalyticsCmdletReporter(string clientID)
+        // Google Analytics Property ID, see di-metrics.
+        private const string PropertyId = "UA-36037335-1";
+        // Application name to report to GA.
+        private const string ApplicationName = "google-cloud-powershell";
+        // Version of the Google.PowerShell assembly.
+        private static readonly Lazy<string> s_appVersion = new Lazy<string>(() => Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+        /// <summary>
+        /// Analytics reporter object.
+        /// </summary>
+        private Lazy<AnalyticsReporter> _reporter = null;
+
+        /// <summary>
+        /// Category for all analytic events reported through this instance.
+        /// </summary>
+        private AnalyticsEventCategory _category = AnalyticsEventCategory.CmdletInvocation;
+
+        public GoogleAnalyticsCmdletReporter(string clientID, AnalyticsEventCategory analyticsCategory)
         {
-            MeasurementProtocolService.SetClientId(clientID);
+            // We report results to Google Analytics if the user has opted-into Cloud SDK metric reporting. However, we
+            // don't want to report data during development. (e.g. when running unit tests.)
+            string envVar = Environment.GetEnvironmentVariable("DISABLE_POWERSHELL_ANALYTICS");
+            bool debugFlag = (envVar == "TRUE");
+
+            _category = analyticsCategory;
+            _reporter = new Lazy<AnalyticsReporter>(() =>
+            {
+                return new AnalyticsReporter(PropertyId,
+                    clientId: clientID,
+                    appName: ApplicationName,
+                    appVersion: s_appVersion.Value,
+                    debug: debugFlag);
+            });
         }
 
         public void ReportSuccess(string cmdletName, string parameterSet)
         {
-            Report(cmdletName, parameterSet, null);
+            Report(_category, cmdletName, parameterSet, null);
         }
 
         public void ReportFailure(string cmdletName, string parameterSet, int errorCode)
         {
-            Report(cmdletName, parameterSet, errorCode);
+            Report(_category, cmdletName, parameterSet, errorCode);
         }
 
-        private void Report(string cmdletName, string parameterSet, int? errorCode)
+        private void Report(AnalyticsEventCategory category, string cmdletName, string parameterSet, int? errorCode)
         {
-            var request = MeasurementProtocolService.GenerateRequest("PowerShell", cmdletName, parameterSet, errorCode);
-            MeasurementProtocolService.IssueRequest(request);
+            _reporter.Value?.ReportEvent(
+                // e.g. CmdletInvocation, Provider
+                category: category.ToString(),
+                // e.g. New-GcsBucket, ConvertTo-GcSqlInstance, Get-ChildItem
+                action: cmdletName,
+                // e.g. FromString, FromFile
+                label: parameterSet,
+                // null (success), 403, 404
+                value: errorCode);
         }
     }
 }
