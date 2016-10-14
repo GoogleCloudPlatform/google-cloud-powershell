6bebce3a25f3c677f5f04516cde625abbf373c5a
quoctruong <quoct@microsoft.com>
2016-10-14 16:38:01 -0700

    Add logic to figure out the active config name

---
+++ Google.PowerShell/Common/GcloudWrapper.cs
@@ -0,0 +1,77 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
+
+namespace Google.PowerShell.Common
+{
+    /// <summary>
+    /// This class wraps the gcloud command and offers up some of its services in a
+    /// as async methods. 
+    /// </summary>
+    public static class GCloudWrapper
+    {
+        /// <summary>
+        /// Returns the 
+        /// </summary>
+        /// <returns></returns>
+        public static string GetInstallationPropertiesPath()
+        {
+            string gCloudInfoOutput = GetGCloudCommandOutput("info");
+            JToken token = JObject.Parse(gCloudInfoOutput);
+
+            try
+            {
+                // SelectToken will throw NullReferenceException if the token cannot be found.
+                token = token.SelectToken("config.paths.installation_properties_path");
+                
+                if (token.Type == JTokenType.String)
+                {
+                    return token.Value<string>();
+                }
+            }
+            catch (NullReferenceException)
+            {
+            }
+
+            throw new FileNotFoundException("Installation Properties file for Google Cloud SDK cannot be found.");
+        }
+
+        private static string GetGCloudCommandOutput(string command, IDictionary<string, string> environment = null)
+        {
+            var actualCommand = $"gcloud {command} --format=json";
+            // This code depends on the fact that gcloud.cmd is a batch file.
+            Debug.Write($"Executing gcloud command: {actualCommand}");
+            ProcessOutput processOutput = ProcessUtils.GetCommandOutput("cmd.exe", $"/c {actualCommand}", environment);
+            if (processOutput.Succeeded)
+            {
+                return processOutput.StandardOutput;
+            }
+
+            if (!string.IsNullOrWhiteSpace(processOutput.StandardError))
+            {
+                throw new InvalidOperationException($"Command {actualCommand} failed with error: processOutput.StandardError");
+            }
+
+            throw new InvalidOperationException($"Command {actualCommand} failed.");
+        }
+    }
+}
---
+++ Google.PowerShell/Common/ProcessUtils.cs
@@ -0,0 +1,175 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Google.PowerShell.Common
+{
+    /// <summary>
+    /// This class contains the output of a running process.
+    /// </summary>
+    public sealed class ProcessOutput
+    {
+        /// <summary>
+        /// Whether the process succeeded or not.
+        /// </summary>
+        public bool Succeeded { get; }
+
+        /// <summary>
+        /// The complete contents of the stderr stream.
+        /// </summary>
+        public string StandardError { get; }
+
+        /// <summary>
+        /// The complete contents of the stdout stream.
+        /// </summary>
+        public string StandardOutput { get; }
+
+        public ProcessOutput(bool succeeded, string standardOutput, string standardError)
+        {
+            Succeeded = succeeded;
+            StandardOutput = standardOutput;
+            StandardError = standardError;
+        }
+    }
+
+    /// <summary>
+    /// The output streams from a process.
+    /// </summary>
+    public enum OutputStream
+    {
+        None,
+        StandardError,
+        StandardOutput
+    }
+
+    /// <summary>
+    /// Evemt args passed to the output handler of a process.
+    /// </summary>
+    public class OutputHandlerEventArgs : EventArgs
+    {
+        public string Line { get; }
+        public OutputStream OutputStream { get; }
+
+        public OutputHandlerEventArgs(string line, OutputStream stream)
+        {
+            Line = line;
+            OutputStream = stream;
+        }
+    }
+
+    /// <summary>
+    /// This class defines helper methods for starting sub-processes and getting the output from
+    /// the processes, including a helper to parse the output as json.
+    /// </summary>
+    public static class ProcessUtils
+    {
+        /// <summary>
+        /// Runs the given binary given by <paramref name="file"/> with the passed in <paramref name="args"/> and
+        /// reads the output of the new process as it happens, calling <paramref name="handler"/> with each line being output
+        /// by the process.
+        /// Uses <paramref name="environment"/> if provided to customize the environment of the child process.
+        /// </summary>
+        /// <param name="file">The path to the binary to execute, it must not be null.</param>
+        /// <param name="args">The arguments to pass to the binary to execute, it can be null.</param>
+        /// <param name="handler">The callback to call with the line being oput by the process, it can be called outside
+        /// of the UI thread. Must not be null.</param>
+        /// <param name="environment">Optional parameter with values for environment variables to pass on to the child process.</param>
+        /// <returns></returns>
+        public static bool RunCommand(
+            string file,
+            string args,
+            EventHandler<OutputHandlerEventArgs> handler,
+            IDictionary<string, string> environment = null)
+        {
+            var startInfo = GetStartInfoForInteractiveProcess(file, args, environment);
+
+            var process = Process.Start(startInfo);
+            ReadLinesFromOutput(OutputStream.StandardError, process.StandardError, handler);
+            ReadLinesFromOutput(OutputStream.StandardOutput, process.StandardOutput, handler);
+            process.WaitForExit();
+            return process.ExitCode == 0;
+        }
+
+        /// <summary>
+        /// Runs a process until it exits, returns it's complete output.
+        /// </summary>
+        /// <param name="file">The path to the exectuable.</param>
+        /// <param name="args">The arguments to pass to the executable.</param>
+        /// <param name="environment">The environment variables to use for the executable.</param>
+        /// <returns></returns>
+        public static ProcessOutput GetCommandOutput(string file, string args, IDictionary<string, string> environment = null)
+        {
+            var startInfo = GetStartInfoForInteractiveProcess(file, args, environment);
+
+            var process = Process.Start(startInfo);
+            var readErrorsTask = process.StandardError.ReadToEndAsync();
+            var readOutputTask = process.StandardOutput.ReadToEndAsync();
+            process.WaitForExit();
+            var succeeded = process.ExitCode == 0;
+            return new ProcessOutput(
+                succeeded: succeeded,
+                standardOutput: readOutputTask.Result,
+                standardError: readErrorsTask.Result);
+        }
+
+        private static ProcessStartInfo GetBaseStartInfo(string file, string args, IDictionary<string, string> environment)
+        {
+            // Always start the tool in the user's home directory, avoid random directories
+            // coming from Visual Studio.
+            ProcessStartInfo result = new ProcessStartInfo
+            {
+                FileName = file,
+                Arguments = args,
+                WorkingDirectory = Environment.GetEnvironmentVariable("USERPROFILE"),
+            };
+
+            // Customize the environment for the incoming process.
+            if (environment != null)
+            {
+                foreach (var entry in environment)
+                {
+                    result.EnvironmentVariables[entry.Key] = entry.Value;
+                }
+            }
+
+            return result;
+        }
+
+        private static ProcessStartInfo GetStartInfoForInteractiveProcess(string file, string args, IDictionary<string, string> environment)
+        {
+            ProcessStartInfo startInfo = GetBaseStartInfo(file, args, environment);
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardInput = true;
+            return startInfo;
+        }
+
+        private static void ReadLinesFromOutput(OutputStream outputStream, StreamReader stream, EventHandler<OutputHandlerEventArgs> handler)
+        {
+            while (!stream.EndOfStream)
+            {
+                var line = stream.ReadLine();
+                handler(null, new OutputHandlerEventArgs(line, outputStream));
+            }
+        }
+    }
+}
