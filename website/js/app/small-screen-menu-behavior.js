ec0223d75575ac461b8c19cc3121423bbf21ccef
Chris Smith <chrsmith@google.com>
2016-10-18 16:22:45 -0700

    Remove top-nav, make left-nav toggleable on small screens

---
+++ website/js/app/small-screen-menu-behavior.js
@@ -0,0 +1,56 @@
+// When the window is small, #header-icon becomes a toggleable menu icon which will show/hide
+// #left-nav. (Which will then float on top of #content-container.) This file implements the
+// behavior of the menu icon.
+(function() {
+    var headerIcon = document.getElementById('header-icon');
+    var leftNav = document.getElementById('left-nav');
+    var contentContainer = document.getElementById('content-container');
+    if (!(headerIcon && leftNav && contentContainer)) {
+        // ERROR: We didn't find the required elements on the page.
+        return;
+    }
+
+    function isScreenSmall() {
+        // Should match body.css, left-nav.css.
+        var windowWidth = document.body.clientWidth;
+        return (windowWidth < 1160);
+    }
+
+    function isNavVisible() {
+        return (leftNav.style.display != 'none');
+    }
+    function setNavDisplay(visible) {
+        leftNav.style.display = visible ? 'block' : 'none';
+    }
+
+    // Register the toggle show/hide behavior.
+    headerIcon.addEventListener('click', function() {
+        if (isScreenSmall()) {
+        var navVisibility = isNavVisible();
+        setNavDisplay(!navVisibility);
+        }      
+    });
+
+    // Handle window resize so the nav is always visible when possible.
+    window.addEventListener('resize', function() {
+        if (!isScreenSmall()) {
+        setNavDisplay(true);
+        }
+    });
+
+    // Start in the collapsed state if the window starts small (e.g. on mobile).
+    window.addEventListener('load', function() {
+        if (isScreenSmall()) {
+        setNavDisplay(false);
+        }
+    });
+
+    // Enable clicking outside of the nav (e.g. content) to close it.
+    contentContainer.addEventListener('click', function() {
+        if (isScreenSmall()) {
+        setNavDisplay(false);
+        }
+    });
+
+    // End of the anonymous JavaScript module.
+})();
