592de420720714fa19f4327708c5aa1145846205
Quoc Truong <quoct@google.com>
2016-12-19 13:39:01 -0800

    Add new-gcpsmessage tests

---
+++ Google.PowerShell.IntegrationTests/PubSub/PubSub.Message.Tests.ps1
@@ -0,0 +1,60 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GCloudCmdlets
+
+$project, $zone, $oldActiveConfig, $configName = Set-GCloudConfig
+
+function CheckMessage($message, $topic)
+{
+    ADD CHECK FOR MESSAGE HERE
+}
+
+Describe "New-GcpsMessage" {
+    It "should work with -Data" {
+        $testData = "Test Data"
+        $gcpsMessage = New-GcpsMessage -Data $testData
+        $gcpsMessage.Data | Should BeExactly $testData
+    }
+
+    It "should work with -Attributes" {
+        $attributes = @{"Key" = "Value"; "Key2" = "Value2"}
+        $gcpsMessage = New-GcpsMessage -Attributes $attributes
+        $gcpsMessage.Attributes["Key"] | Should BeExactly "Value"
+        $gcpsMessage.Attributes["Key2"] | Should BeExactly "Value2"
+    }
+
+    It "should work with both -Data and -Attributes" {
+        $testData = "Test Data"
+        $attributes = @{"Key" = "Value"; "Key2" = "Value2"}
+        $gcpsMessage = New-GcpsMessage -Data $testData -Attributes $attributes
+        $gcpsMessage.Data | Should BeExactly $testData
+        $gcpsMessage.Attributes["Key"] | Should BeExactly "Value"
+        $gcpsMessage.Attributes["Key2"] | Should BeExactly "Value2"
+    }
+
+    It "should throw error if there are no data or attributes" {
+        { New-GcpsMessage } | Should Throw "Cannot construct a PubSub message"
+    }
+}
+
+Describe "Publish-GcpsMessage" {
+    It "should work with -Data" {
+        $r = Get-Random
+        $topicName = "gcp-test-new-subscription-topic-$r"
+        $subscriptionName = "gcp-test-new-subscription-$r"
+        $testData = "Test Data"
+
+        try {
+            New-GcpsTopic -Topic $topicName
+            New-GcpsSubscription -Subscription $subscriptionName -Topic $topicName
+
+            $publishedMessage = Publish-GcpsMessage -Data $testData
+            $publishedMessage.MessageId | Should Not BeNullOrEmpty
+
+            ADD MORE CHECK FOR MESSAGE HERE
+        }
+        finally {
+            Remove-GcpsTopic $topicName
+            Remove-GcpsSubscription $subscriptionName
+        }
+    }
+}
--- Google.PowerShell/PubSub/GcpsMessage.cs
+++ Google.PowerShell/PubSub/GcpsMessage.cs
@@ -6,17 +6,52 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using System.Net;
 using System.Text;
 
 namespace Google.PowerShell.PubSub
 {
+    /// <summary>
+    /// <para type="synopsis">
+    /// Creates a Google Cloud PubSub message.
+    /// </para>
+    /// <para type="description">
+    /// Creates a Google Cloud PubSub message. The message created is intended to be used with
+    /// Publish-GcpsMessage cmdlet to publish it to a topic. The message payload must not be empty;
+    /// it must contain either a non-empty data field or at least one attribute.
+    /// The cmdlet will base64-encode the message data.
+    /// </para>
+    /// <example>
+    ///   <code>PS C:\> New-GcpsMessage -Data "my-data"</code>
+    ///   <para>This command creates a new message with data "my-data".</para>
+    /// </example>
+    /// <example>
+    ///   <code>PS C:\> New-GcpsMessage -Data "my-data" -Attributes @{"key"="value"}</code>
+    ///   <para>
+    ///   This command creates a new message with data "my-data" and an attribute pair "key", "value".
+    ///   </para>
+    /// </example>
+    /// <para type="link" uri="(https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage)">
+    /// [PubSub Message]
+    /// </para>
+    /// </summary>
     [Cmdlet(VerbsCommon.New, "GcpsMessage")]
     public class NewGcpsMessage : GcpsCmdlet
     {
+        /// <summary>
+        /// <para type="description">
+        /// The message payload. This will be base64-encoded by the cmdlet.
+        /// </para>
+        /// </summary>
         [Parameter(Mandatory = false)]
         [ValidateNotNullOrEmpty]
         public string Data { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// Optional attributes for this message.
+        /// </para>
+        /// </summary>
         [Parameter(Mandatory = false)]
         [ValidateNotNullOrEmpty]
         public Hashtable Attributes { get; set; }
@@ -30,25 +65,50 @@ namespace Google.PowerShell.PubSub
         internal static PubsubMessage ConstructPubSubMessage(string data, Hashtable attributes)
         {
             Dictionary<string, string> attributesDict = null;
-            string base64EncodedMessage = null;
             if (attributes != null && attributes.Count > 0)
             {
                 attributesDict = ConvertToDictionary<string, string>(attributes);
             }
-            if (!string.IsNullOrWhiteSpace(data))
-            {
-                base64EncodedMessage = Convert.ToBase64String(Encoding.UTF8.GetBytes(data));
-            }
             // A valid PubSub message must have either a non-empty message or a non-empty attributes.
-            if (attributesDict == null && base64EncodedMessage == null)
+            if (attributesDict == null && data == null)
             {
-                throw new ArgumentException("Cannot construct a PubSub message because both the message and the attributes are empty.");
+                throw new ArgumentException("Cannot construct a PubSub message because both the message data and the attributes are empty.");
             }
-            PubsubMessage psMessage = new PubsubMessage() { Data = base64EncodedMessage, Attributes = attributesDict };
+            PubsubMessage psMessage = new PubsubMessage() { Data = data, Attributes = attributesDict };
             return psMessage;
         }
     }
 
+    /// <summary>
+    /// <para type="synopsis">
+    /// Publishes one or more PubSub messages to a topic.
+    /// </para>
+    /// <para type="description">
+    /// Publishes one or more PubSub messages to a topic. Will raise errors if the topic does not exist.
+    /// The cmdlet will search for the topic in the default project if -Project is not used.
+    /// To publish more than 1 message, use -Message parameter with an array of messages constructed from New-GcpsMessage.
+    /// Otherwise, use -Data and -Attribute parameters to publish a single message.
+    /// </para>
+    /// <example>
+    ///   <code>PS C:\> Publish-GcpsTopic -Topic "my-topic" -Data "This is a test." -Attributes @{"key"="value"}</code>
+    ///   <para>
+    ///   This command publishes a message with the specified data and attribute to topic "my-topic" in the default project.
+    ///   </para>
+    /// </example>
+    /// <example>
+    ///   <code>
+    ///   PS C:\> $message1 = New-GcpsMessage -Data "my-data"
+    ///   PS C:\> $message2 = New-GcpsMessage -Data "my-data2" -Attributes @{"key"="test"}
+    ///   PS C:\> Publish-GcpsTopic -Topic "my-topic" -Message $message1, $message2 -Project "my-project"
+    ///   </code>
+    ///   <para>
+    ///   This command publishes 2 messages to topic "my-topic" in the project "my-project".
+    ///   </para>
+    /// </example>
+    /// <para type="link" uri="(https://cloud.google.com/pubsub/docs/publisher#publish-messages-to-a-topic)">
+    /// [Publishing Messages to a Topic]
+    /// </para>
+    /// </summary>
     [Cmdlet(VerbsData.Publish, "GcpsMessage", DefaultParameterSetName = ParameterSetNames.DataAndAttributes)]
     public class PublishGcpsMessage : GcpsCmdlet
     {
@@ -60,7 +120,7 @@ namespace Google.PowerShell.PubSub
 
         /// <summary>
         /// <para type="description">
-        /// The project to check for log entries. If not set via PowerShell parameter processing, will
+        /// The project to check for topic. If not set via PowerShell parameter processing, will
         /// default to the Cloud SDK's DefaultProject property.
         /// </para>
         /// </summary>
@@ -68,18 +128,38 @@ namespace Google.PowerShell.PubSub
         [ConfigPropertyName(CloudSdkSettings.CommonProperties.Project)]
         public string Project { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// The topic to which the messages will be published.
+        /// </para>
+        /// </summary>
         [Parameter(Mandatory = true, Position = 0)]
         [ValidateNotNullOrEmpty]
         public string Topic { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// Data of the message that will be published.
+        /// </para>
+        /// </summary>
         [Parameter(Mandatory = false, ParameterSetName = ParameterSetNames.DataAndAttributes)]
         [ValidateNotNullOrEmpty]
         public string Data { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// Attributes of the message that will be published.
+        /// </para>
+        /// </summary>
         [Parameter(Mandatory = false, ParameterSetName = ParameterSetNames.DataAndAttributes)]
         [ValidateNotNullOrEmpty]
         public Hashtable Attributes { get; set; }
 
+        /// <summary>
+        /// <para type="description">
+        /// Messages to be published. Use this parameter to publish one or more messages.
+        /// </para>
+        /// </summary>
         [Parameter(Mandatory = true, ParameterSetName = ParameterSetNames.Message)]
         [ValidateNotNullOrEmpty]
         public PubsubMessage[] Message { get; set; }
@@ -87,20 +167,50 @@ namespace Google.PowerShell.PubSub
         protected override void ProcessRecord()
         {
             Topic = GetProjectPrefixForTopic(Topic, Project);
-            PublishRequest requestBody = null;
             if (ParameterSetName == ParameterSetNames.DataAndAttributes)
             {
                 PubsubMessage psMessage = NewGcpsMessage.ConstructPubSubMessage(Data, Attributes);
-                requestBody = new PublishRequest() { Messages = new List<PubsubMessage>() { psMessage } };
+                Message = new PubsubMessage[] { psMessage };
             }
-            else
-            {
-                requestBody = new PublishRequest() { Messages = Message };
-            }
-            ProjectsResource.TopicsResource.PublishRequest publishRequest = Service.Projects.Topics.Publish(requestBody, Topic);
-            PublishResponse response = publishRequest.Execute();
 
-            WriteObject(response.MessageIds, true);
+            // Encode data in each message to base64.
+            foreach (PubsubMessage psMessage in Message)
+            {
+                psMessage.Data = Convert.ToBase64String(Encoding.UTF8.GetBytes(psMessage.Data));
+            }
+
+            PublishRequest requestBody = new PublishRequest() { Messages = Message };
+            ProjectsResource.TopicsResource.PublishRequest publishRequest = Service.Projects.Topics.Publish(requestBody, Topic);
+            try
+            {
+                PublishResponse response = publishRequest.Execute();
+
+                // The MessageIds is a server-assigned ID of the published message.
+                // They are in the same order as the messages in the request.
+                // We will use them to populate each message in the request with the corresponding message id.
+                IList<string> returnedMessageIds = response.MessageIds;
+                if (returnedMessageIds.Count != requestBody.Messages.Count)
+                {
+                    throw new InvalidOperationException($"Only {returnedMessageIds.Count} out of {requestBody.Messages.Count} is published.");
+                }
+
+                for (int index = 0; index < returnedMessageIds.Count; index += 1)
+                {
+                    PubsubMessage publishedMessage = requestBody.Messages[index];
+                    publishedMessage.MessageId = returnedMessageIds[0];
+                    byte[] decodedMessageDataBytes = Convert.FromBase64String(publishedMessage.Data);
+                    publishedMessage.Data = Encoding.UTF8.GetString(decodedMessageDataBytes);
+                    WriteObject(publishedMessage);
+                    index += 1;
+                }
+            }
+            catch (GoogleApiException ex) when (ex.HttpStatusCode == HttpStatusCode.NotFound)
+            {
+                WriteResourceMissingError(
+                    exceptionMessage: $"Topic '{Topic}' does not exist in project '{Project}'.",
+                    errorId: "TopicNotFound",
+                    targetObject: Topic);
+            }
         }
     }
 }
