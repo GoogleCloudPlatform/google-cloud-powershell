e65ad6e82271b5cb269e4a3216c6a505603da55f
Alex Handley <alexhandley@google.com>
2017-02-08 12:27:04 -0800

    Initial commit for project cmdlet

---
+++ Google.PowerShell.IntegrationTests/BigQuery/GcbqCmdlets.ps1
@@ -0,0 +1,16 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+# Define variables that will be used in testing
+$project = "gcloud-powershell-testing"
+
+$nonExistProject = "project-no-exist"
+$accessErrProject = "asdf"
+
+# Define functions that will be used in testing
+
+function Test-Funct($fileName) {
+    if (Test-Path $fileName) {
+        Remove-Item $fileName
+    }
+}
