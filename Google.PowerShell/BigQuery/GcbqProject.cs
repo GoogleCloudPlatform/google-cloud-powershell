f316d70e82ad8d3e10fba357a6ece77456bdb9b7
Alex Handley <alexhandley@google.com>
2017-02-09 13:17:09 -0800

    revisions to Get-GcbqProject based on PR comments

--- Google.PowerShell/BigQuery/GcbqProject.cs
+++ Google.PowerShell/BigQuery/GcbqProject.cs
@@ -1,4 +1,7 @@
-﻿using Google.Apis.Bigquery.v2;
+﻿// Copyright 2015-2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using Google.Apis.Bigquery.v2;
 using Google.Apis.Bigquery.v2.Data;
 using System.Management.Automation;
 using System;
@@ -7,20 +10,15 @@ namespace Google.PowerShell.BigQuery
 {
     /// <summary>
     /// <para type="synopsis">
-    /// Lists all projects to which you have been granted any project role.
+    /// Lists all Google Cloud Platform projects that you have permission for.
     /// </para>
     /// <para type="description">
-    /// Queries to find which projects you have been granted any project role in and returns them in the form of ProjectsData objects.
-    /// The flag -MaxResults can be used to specify the maximum number of results retrieved at once, and -PageToken can be used to
-    /// specify an offset into the project list at which to start.
+    /// Queries to find which Google Cloud Platform projects you have been granted any project role in and writes them to the pipeline.
+    /// Output is in the form of ProjectData objects that you can then filter before sending to other commands.
     /// </para>
     /// <example>
-    ///   <code>PS C:\> Get-GcbqProjects -MaxResults 20</code>
-    ///   <para>This command will list up to the first 20 BigQuery projects that you've been granted any roles for.</para>
-    /// </example>
-    /// <example>
-    ///   <code>PS C:\> Get-GcbqProjects -MaxResults 20 -PageToken 20</code>
-    ///   <para>This command will list the second page of up to 20 BigQuery projects that you've been granted any roles for.</para>
+    ///   <code>PS C:\> Get-GcbqProject</code>
+    ///   <para>This command will list all of the GCP projects that you've been granted any roles for.</para>
     /// </example>
     /// <para type="link" uri="(https://cloud.google.com/bigquery/docs/reference/rest/v2/projects)">
     /// [BigQuery Projects]
@@ -29,39 +27,12 @@ namespace Google.PowerShell.BigQuery
     [Cmdlet(VerbsCommon.Get, "GcbqProject")]
     public class GetGcbqProject : GcbqCmdlet
     {
-        /// <summary>
-        /// <para type="description">
-        /// The maximum number of results to return.
-        /// </para>
-        /// </summary>
-        [Alias("mr")]
-        [Parameter(Mandatory = false)]
-        public uint? MaxResults { get; set; }
-
-        /// <summary>
-        /// <para type="description">
-        /// Page token, returned by a previous call, to request the next page of results.
-        /// </para>
-        /// </summary>
-        [Alias("pt")]
-        [Parameter(Mandatory = false)]
-        public string PageToken { get; set; }
-
         protected override void ProcessRecord()
         {
-            //check for valid value of MaxResults
-            if (MaxResults <= 0)
-            {
-                ThrowTerminatingError(new ErrorRecord(new Exception("MaxResults only takes positive numbers."), 
-                    "400", ErrorCategory.InvalidArgument, MaxResults));
-            }
-
-            //create request with flag results.  nulls should be handled
+            // Create the project list request
             ProjectsResource.ListRequest request = Service.Projects.List();
-            request.MaxResults = MaxResults;
-            request.PageToken = PageToken;
 
-            //send request and parse response
+            // Send request and parse response, iterating if needed.
             do
             {
                 ProjectList response = request.Execute();
