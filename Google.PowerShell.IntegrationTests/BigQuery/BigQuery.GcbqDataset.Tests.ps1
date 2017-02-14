ff387b97187010d241a89e7f27f64cfbe95981cf
Alex Handley <alexhandley@google.com>
2017-02-14 13:04:53 -0800

    edits from PR comments

--- Google.PowerShell.IntegrationTests/BigQuery/BigQuery.GcbqDataset.Tests.ps1
+++ Google.PowerShell.IntegrationTests/BigQuery/BigQuery.GcbqDataset.Tests.ps1
@@ -10,54 +10,54 @@ Describe "New-GcbqDataset" {
 		$data.FriendlyName | Should Be "Testdata"
 		$data.Description | Should Be "Some interesting data!"
 		$data.DefaultTableExpirationMs | Should Be 86400000
-    }
+	}
 
 	It "should accept a dataset object from pipeline" {
 		$data = New-Object -TypeName Google.Apis.Bigquery.v2.Data.Dataset
 		$data.DatasetReference = New-Object -TypeName Google.Apis.Bigquery.v2.Data.DatasetReference
-        $data.DatasetReference.DatasetId = "test_data_id2"
-        $data.DatasetReference.ProjectId = $project
+		$data.DatasetReference.DatasetId = "test_data_id2"
+		$data.DatasetReference.ProjectId = $project
 		$data.FriendlyName = "PipeTest"
 		$data.Description = "Some cool data coming hot off the pipeline!"
 		$data | New-GcbqDataset -outvariable newdata
 		$newdata | Should Not BeNullOrEmpty
 		$newdata.DatasetReference.DatasetId | Should Be "test_data_id2"
 		$newdata.FriendlyName | Should Be "PipeTest"
-    }
+	}
 
 	It "should accept a dataset object from a command line argument" {
 		$data = New-Object -TypeName Google.Apis.Bigquery.v2.Data.Dataset
 		$data.DatasetReference = New-Object -TypeName Google.Apis.Bigquery.v2.Data.DatasetReference
-        $data.DatasetReference.DatasetId = "test_data_id3"
-        $data.DatasetReference.ProjectId = $project
+		$data.DatasetReference.DatasetId = "test_data_id3"
+		$data.DatasetReference.ProjectId = $project
 		$data.FriendlyName = "PipeTest"
 		$data.Description = "Some cool data coming hot off the pipeline!"
 		$newdata = New-GcbqDataset -Dataset $data
 		$newdata | Should Not BeNullOrEmpty
 		$newdata.DatasetReference.DatasetId | Should Be "test_data_id3"
 		$newdata.FriendlyName | Should Be "PipeTest"
-    }
+	}
 
 	It "should not work with no arguments (meaning no datasetId)" {
 		$data = New-GcbqDataset | Should Throw
-    }
+	}
 
 	It "should reject datasets that do not have unique ids" {
 		$data = New-Object -TypeName Google.Apis.Bigquery.v2.Data.Dataset
 		$data.DatasetReference = New-Object -TypeName Google.Apis.Bigquery.v2.Data.DatasetReference
-        $data.DatasetReference.DatasetId = "test_data_id6"
+		$data.DatasetReference.DatasetId = "test_data_id6"
 		$data2 = New-Object -TypeName Google.Apis.Bigquery.v2.Data.Dataset
 		$data2.DatasetReference = New-Object -TypeName Google.Apis.Bigquery.v2.Data.DatasetReference
-        $data2.DatasetReference.DatasetId = "test_data_id6"
+		$data2.DatasetReference.DatasetId = "test_data_id6"
 		$data | New-GcbqDataset
 		{ $data2 | New-GcbqDataset } | Should Throw
-    }
+	}
 
 	It "should use the -Project tag correctly" {
 		$data = New-GcbqDataset -Project $project "test_data_id7" -Name "Testdata" -Description "Some interesting data!"
 		$data | Should Not BeNullOrEmpty
 		$data.DatasetReference.ProjectId | Should Be $project
-    }
+	}
 
 	It "should throw when you try to add to a project that doesnt exist" {
 		{ New-GcbqDataset -Project $nonExistProject "test_data_id8" -Name "Testdata" -Description "Some interesting data!" } | Should Throw
@@ -65,7 +65,7 @@ Describe "New-GcbqDataset" {
 
 	It "should throw when you try to add to a project that is not yours" {
 		{ New-GcbqDataset -Project $accessErrProject "test_data_id9" -Name "Testdata" -Description "Some interesting data!" } | Should Throw
-    }
+	}
 
 	#add afterall block that deletes test datasets when remove-x command is done
 }
