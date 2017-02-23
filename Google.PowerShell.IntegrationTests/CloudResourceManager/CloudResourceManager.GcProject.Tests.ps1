e6b5357eeda8c7f2f1eace36f9ff4fe81e58a7e5
Quoc Truong <quoct@google.com>
2017-02-23 13:37:55 -0800

    Add Get-GcProject cmdlet; Split SQL tests

---
+++ Google.PowerShell.IntegrationTests/CloudResourceManager/CloudResourceManager.GcProject.Tests.ps1
@@ -0,0 +1,65 @@
+﻿. $PSScriptRoot\..\GcloudCmdlets.ps1
+Install-GcloudCmdlets
+
+$script:powerShellProjectId = "gcloud-powershell-testing"
+$script:powerShellProjectTwoId = "gcloud-powershell-testing-2"
+$script:powerShellProjectName = "Google Powershell Testing"
+$script:powerShellProjectTwoName = "GCloud PowerShell Testing 2"
+
+Describe "Get-GcIamPolicyBinding" {
+    It "should work" {
+        $projects = Get-GcProject
+        $project = $projects | Where-Object {$_.ProjectId -eq $powerShellProjectId}
+        $projectTwo = $projects | Where-Object {$_.ProjectId -eq $powerShellProjectTwoId}
+
+        $project.Name | Should BeExactly $powerShellProjectName
+        $project.LifecycleState | Should Be ACTIVE
+        $project.Labels | Should Not BeNullOrEmpty
+        $projectTwo.Name | Should BeExactly $powerShellProjectTwoName
+        $projectTwo.LifecycleState | Should Be ACTIVE
+        $projectTwo.Labels | Should Not BeNullOrEmpty
+    }
+
+    It "should work with -Name" {
+        $project = Get-GcProject -Name $powerShellProjectName
+        $project.Name | Should BeExactly $powerShellProjectName
+        $project.ProjectId | Should BeExactly $powerShellProjectId
+    }
+
+    It "should work with -ProjectId" {
+        $project = Get-GcProject -ProjectId $powerShellProjectTwoId
+        $project.Name | Should BeExactly $powerShellProjectTwoName
+        $project.ProjectId | Should BeExactly $powerShellProjectTwoId
+    }
+
+    It "should work with -Label" {
+        # Both projects have this label.
+        $commonLabel = @{"gcloud-powershell-testing" = "testing"}
+        $projects = Get-GcProject -Label $commonLabel
+        $project = $projects | Where-Object {$_.ProjectId -eq $powerShellProjectId}
+        $projectTwo = $projects | Where-Object {$_.ProjectId -eq $powerShellProjectTwoId}
+
+        $projects.Count | Should Be 2
+        $project.Name | Should BeExactly $powerShellProjectName
+        $projectTwo.Name | Should BeExactly $powerShellProjectTwoName
+    }
+
+    It "should work with multiple labels" {
+        # Only the second project has both of these labels.
+        $labels = @{"gcloud-powershell-testing" = "testing"; "powershell" = "gcloud"}
+        $project = Get-GcProject -Label $labels
+        $project.Name | Should BeExactly $powerShellProjectTwoName
+        $project.ProjectId | Should BeExactly $powerShellProjectTwoId
+    }
+
+    It "should work with multiple parameters" {
+        $commonLabel = @{"gcloud-powershell-testing" = "testing"}
+        $project = Get-GcProject -Name $powerShellProjectName -Label $commonLabel
+        $project.Name | Should BeExactly $powerShellProjectName
+        $project.ProjectId | Should BeExactly $powerShellProjectId
+
+        $projectTwo = Get-GcProject -ProjectId $powerShellProjectId -Label $commonLabel
+        $project.Name | Should BeExactly $powerShellProjectName
+        $project.ProjectId | Should BeExactly $powerShellProjectId
+    }
+}
---
+++ Google.PowerShell/CloudResourceManager/GoogleCloudProjectCmdlets.cs
@@ -0,0 +1,133 @@
+﻿using Google.Apis.CloudResourceManager.v1;
+using Google.Apis.CloudResourceManager.v1.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Google.PowerShell.CloudResourceManager
+{
+    /// <summary>
+    /// <para type="synopsis">
+    /// Retrieves one or more Google Cloud projects.
+    /// </para>
+    /// <para type="description">
+    /// Retrieves one or more Google Cloud projects. The cmdlet will return all available projects if no parameter
+    /// is used. If -Name, -Id or -Label is used, the cmdlets will return the projects that match the given arguments.
+    /// </para>
+    /// <example>
+    ///   <code>PS C:\> Get-GcProject</code>
+    ///   <para>This command gets all the available Google Cloud projects.</para>
+    /// </example>
+    /// <example>
+    ///   <code>PS C:\> Get-GcProject -Name "My Project"</code>
+    ///   <para>This command gets the project that has the name "My Project".</para>
+    /// </example>
+    /// <example>
+    ///   <code>PS C:\> Get-GcProject -Id "my-id"</code>
+    ///   <para>This command gets the project that has the Id "My Project".</para>
+    /// </example>
+    /// <example>
+    ///   <code>PS C:\> Get-GcProject -Label {"environment" = "test"}</code>
+    ///   <para>This command gets all the projects that has the label "environment" with value "test".</para>
+    /// </example>
+    /// <para
+    ///     type="link"
+    ///     uri="(https://cloud.google.com/resource-manager/docs/cloud-platform-resource-hierarchy#cloud_platform_projects)">
+    /// [Projects]
+    /// </para>
+    /// <para type="link" uri="(https://cloud.google.com/resource-manager/docs/using-labels)">
+    /// [Labels]
+    /// </para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GcProject")]
+    public class GetGcProjectCmdlet : CloudResourceManagerCmdlet
+    {
+        /// <summary>
+        /// <para type="description">
+        /// The name of the project to seach for.
+        /// This parameter is case insensitive.
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// <para type="description">
+        /// The Id of the project to seach for.
+        /// This parameter is case insensitive.
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        [ValidateNotNullOrEmpty]
+        public string ProjectId { get; set; }
+
+        /// <summary>
+        /// <para type="description">
+        /// The labels of the project to seach for.
+        /// Key and value of the label should be in lower case with no spaces in them.
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        [ValidateNotNullOrEmpty]
+        public Hashtable Label { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            // Filter is case insensitive.
+            string filter = string.Empty;
+            if (Name != null)
+            {
+                filter = $"name:'{Name}'";
+            }
+
+            if (ProjectId != null)
+            {
+                filter += $" id:'{ProjectId}'";
+            }
+
+            if (Label != null && Label.Count > 0)
+            {
+                // Each label is represented in the filter as labels.key:value.
+                foreach (string key in Label.Keys)
+                {
+                    string value = Label[key]?.ToString();
+                    if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(value))
+                    {
+                        throw new PSArgumentException(
+                            "Label dictionary should not have null or empty key or value",
+                            nameof(Label));
+                    }
+
+                    // The key and value needs to be in lower case and they cannot have space.
+                    string formattedKey = key.Trim().ToLower();
+                    value = value.Trim().ToLower();
+                    if (formattedKey.Contains(" ") || value.Contains(" "))
+                    {
+                        throw new PSArgumentException("Label key and value cannot contain white spaces", nameof(Label));
+                    }
+
+                    filter += $" labels.{key.ToLower()}:{value.ToLower()}";
+                }
+            }
+            filter = filter.Trim().Replace('\'', '"');
+
+            ProjectsResource.ListRequest request = Service.Projects.List();
+            if (!string.IsNullOrWhiteSpace(filter))
+            {
+                request.Filter = filter;
+            }
+
+            do
+            {
+                ListProjectsResponse response = request.Execute();
+                if (response.Projects != null)
+                {
+                    WriteObject(response.Projects, true);
+                }
+                request.PageToken = response.NextPageToken;
+            }
+            while (!Stopping && request.PageToken != null);
+        }
+    }
+}
